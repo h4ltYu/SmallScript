f9b3edc964f4cf93d18fc1aabd6da7d085b72456
h4ltYu <h4ltYu@users.noreply.github.com>
2019-08-09 01:41:34 +0700

    Fix bug and improve performance

--- getDomainsPointToTheSameIP.py
+++ getDomainsPointToTheSameIP.py
@@ -2,6 +2,7 @@ import time
 import queue
 import requests
 from publicdns.client import PublicDNS
+import concurrent.futures
 
 APIKEY=["<List virustotal apikey come here>","APIKEY2"]
 
@@ -11,7 +12,6 @@ TimeToSleep = 60/(NumOfAPIKey*MaxReqPerMin)
 domainQueue = queue.Queue()
 ipQueue = queue.Queue()
 domainSet = set()
-ipSet = set()
 ID = {}
 originIp = set()
 
@@ -19,21 +19,36 @@ def lookup():
     global ID
     global domainQueue
     global ipQueue
-    while not domainQueue.empty():
-        domain = domainQueue.get()
-        print("processing domain: %s" %(domain))
-        client = PublicDNS()
-        try:
-            ips = client.resolve(domain)
-        except Exception as e:
-            print(e)
-            continue
-        for ip in ips:
-            if(ip in ID):
-                ID[ip].add((domain))
-            else:
-                ID[ip] = set((domain))
-                ipQueue.put(ip)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
+        listDomain = []
+        while not domainQueue.empty():
+            listDomain.append(domainQueue.get())
+        future_to_ips = {executor.submit(resolve, domain) : domain for domain in listDomain}
+        for future in concurrent.futures.as_completed(future_to_ips):
+            domain = future_to_ips[future]
+            try:
+                ips = future.result()
+            except Exception as exc:
+                continue
+            if(ips is None):
+                continue
+            for ip in ips:
+                if(ip in ID):
+                    ID[ip] |= {domain}
+                else:
+                    ID[ip] = set({domain})
+                    ipQueue.put(ip)
+
+def resolve(domain):
+    print("processing domain: %s" %(domain))
+    if domain is None:
+        return None
+    client = PublicDNS()
+    try:
+        ips = client.resolve(domain)
+    except Exception as e:
+        return None
+    return ips
 
 def reverseLookup():
     global domainSet
